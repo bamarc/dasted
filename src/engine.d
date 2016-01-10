e3086fe38cffd2e332a50b22f4e2973cf6174e9e
Maksim Klimov <cleemmi@gmail.com>
2016-01-09 22:05:18 -0800

    Add complete method into Engine

--- src/engine.d
+++ src/engine.d
@@ -127,4 +127,77 @@ public:
         }
         return candidates.empty() ? null : candidates.front();
     }
+
+    ISymbol[] complete(Offset pos)
+    {
+        debug trace("offset = ", pos);
+        auto parent = findScope(pos);
+        auto tokens = getIdentifierChain(getBeforeTokens(pos));
+        return complete(parent, tokens, pos);
+    }
+
+    ISymbol[] complete(ISymbol scopeSymbol,
+        const(Token)[] identifierChain, Offset limit)
+    {
+        debug trace("chain = ", map!(a => txt(a))(identifierChain));
+        if (identifierChain.empty())
+        {
+            return null;
+        }
+
+        auto s = TokenStream(identifierChain);
+        bool isExact()
+        {
+            return s.hasNext();
+        }
+        string stxt(const(Token) t)
+        {
+            auto s = txt(t);
+            auto offs = offset(t);
+            if (offs + s.length < limit)
+            {
+                return s;
+            }
+            return s[0..limit  - offs];
+        }
+        if (s.curr.type == tok!".")
+        {
+            scopeSymbol = activeModule();
+            if (!s.next())
+            {
+                return null;
+            }
+        }
+        ISymbol[] candidates = scopeSymbol.findInScope(stxt(s.curr), isExact());
+        while (s.next())
+        {
+            if (candidates.empty())
+            {
+                return null;
+            }
+            auto nextToken = identifierChain.back();
+            identifierChain.popBack();
+            if (nextToken == tok!".")
+            {
+                if (candidates.length != 1)
+                {
+                    return null;
+                }
+                candidates = candidates.front().dotAccess();
+            }
+            else if (nextToken == tok!"identifier")
+            {
+                candidates = isExact() ?
+                    filter!(a => a.name() == stxt(nextToken))(
+                        candidates).array()
+                    : filter!(a => a.name().startsWith(stxt(nextToken)))(
+                          candidates).array();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return candidates;
+    }
 }
