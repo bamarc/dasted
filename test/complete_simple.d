d3b116e50f404c032e82441f6d289f64238b1a8f
Maksim Klimov <cleemmi@gmail.com>
2016-02-20 12:20:34 -0800

    Add tests

--- test/complete_simple.d
+++ test/complete_simple.d
@@ -30,8 +30,8 @@ unittest
             C |a_member; // 7
         }
         const(A.B.C) c = null;
-        c.|c_|member.|a|b = 5;
-        EOF)";
+        c.|c_mem|ber.|b_|member = 5;
+        )";
     auto srcPos = getSourcePos(sources);
     engine.setSource("test", srcPos.src, 0);
 
@@ -58,4 +58,43 @@ unittest
     assert(symbol_b_dot[1] == false);
     assert(symbol_b_dot[0].length == 4);
     assert(symbol_b_dot[0].map!(a => a.name()).isPermutation(["foo", "foo", "C", "b_member"]));
+    assert(symbol_b_dot[0].filter!(a => a.name() == "foo")
+                          .map!(a => a.position)
+                          .isPermutation([srcPos.pos[1], srcPos.pos[2]]));
+    assert(symbol_b_dot[0].filter!(a => a.name() == "C")
+                          .map!(a => a.position)
+                          .equal([srcPos.pos[4]]));
+
+    auto symbol_b_complete = engine.complete(srcPos.pos[11]);
+    assert(symbol_b_complete[1] == false);
+    assert(symbol_b_complete[0].length == 1);
+    assert(symbol_b_complete[0][0].name() == "b_member");
+}
+
+unittest
+{
+    Engine engine = new Engine;
+    string sources = q"(
+        class |ISimple
+        {
+            ISimple |foo(int a, ISimple b);
+            const(ISimple) |foo(ISimple c) const;
+        }
+
+        ISimple a_inst = new ISimple;
+        auto res = a_inst.|foo(|0, a|_inst);
+        )";
+    auto srcPos = getSourcePos(sources);
+    engine.setSource("test", srcPos.src, 0);
+
+    auto symbols = engine.complete(srcPos.pos[3]);
+
+    assert(symbols[1] == false);
+    assert(symbols[0].length == 2);
+    assert(symbols[0].map!(a => a.name()).equal(["foo", "foo"]));
+    assert(symbols[0].map!(a => a.position).isPermutation([srcPos.pos[1], srcPos.pos[2]]));
+
+    auto symbol_b_complete = engine.complete(srcPos.pos[4]);
+    //assert(symbol_b_complete[1] == true);
+    //assert(symbol_b_complete[0].length == 2);
 }
